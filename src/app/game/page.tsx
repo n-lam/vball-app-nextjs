7633e4f2869de621c621b01c6dffb85ac7c37b18
Nathan Lam <nathan.lam@grokacademy.org>
2023-03-20 01:41:50 +1100

    Fix styles and create home page

---
+++ src/app/game/page.tsx
@@ -0,0 +1,190 @@
+"use client";
+import styles from "./game.module.css";
+
+import { create } from "zustand";
+
+type Player = "S" | "OH1" | "MB1" | "OP" | "OH2" | "MB2";
+
+type Side = 'home' | 'away';
+
+type SideData = {
+  [key in Side]: {
+    players: Player[];
+    score: number;
+  }
+}
+
+type GameStore = SideData & {
+  servingSide: Side;
+  rotatePlayers: (side: Side, forward?: boolean) => void;
+  updateScore: (action: "add" | "sub", side: Side) => void;
+};
+
+const _rotatePlayers = (players: Player[], forward=true) => {
+  const teamPlayers = [...players];
+  if (forward) {
+    teamPlayers.push(teamPlayers.shift() as Player);
+  } else {
+    teamPlayers.unshift(teamPlayers.pop() as Player);
+  }
+  return teamPlayers;
+}
+
+const useStore = create<GameStore>((set) => ({
+  home: {
+    players: ["S", "OH1", "MB1", "OP", "OH2", "MB2"],
+    score: 0,
+  },
+  away: {
+    players: ["S", "OH1", "MB1", "OP", "OH2", "MB2"],
+    score: 0,
+  },
+  servingSide: "home",
+  rotatePlayers: (side, forward = true) =>
+    set(({ [side]: teamData}) => {
+      // const playerKeyname: "homePlayers" | "awayPlayers" = `${side}Players`;
+      const teamPlayers = _rotatePlayers(teamData.players, forward);
+      return {
+        [side]: {
+          ...teamData,
+          players: teamPlayers,
+        },
+      };
+    }),
+  updateScore: (action = "add", side) =>
+    set(({ [side]: teamData, servingSide }) => {
+      // const scoreKeyname: "homeScore" | "awayScore" = `${side}Score`;
+      const oldScore = teamData.score;
+      const newScore = action === "add" ? oldScore + 1 : oldScore - 1;
+      const correctedScore = newScore < 0 ? 0 : newScore;
+
+      const changeServer = action === "add" && side !== servingSide;
+      const nextServingSide = changeServer ? { servingSide: side } : {};
+
+      const players = changeServer ? _rotatePlayers(teamData.players) : teamData.players;
+
+      return {
+        ...nextServingSide,
+        [side]: {
+          ...teamData,
+          players,
+          score: correctedScore,
+        },
+      };
+    }),
+}));
+
+type ScoreboardProps = {
+  side: "home" | "away";
+};
+
+function Scoreboard({ side }: ScoreboardProps) {
+  const { [side]: teamData, updateScore } = useStore();
+  const { score } = teamData;
+
+  return (
+    <div
+      className={[
+        styles.scoreboard__container,
+        styles[`scoreboard__${side}`],
+      ].join(" ")}
+    >
+      <h2 className={styles.scoreboard__team_name}>{side}</h2>
+      <div className={styles.scoreboard}>
+        <button
+          className={styles.scoreboard__button}
+          onClick={() => updateScore("sub", side)}
+        >
+          -
+        </button>
+        <p className={styles.scoreboard__score}>{score}</p>
+        <button
+          className={styles.scoreboard__button}
+          onClick={() => updateScore("add", side)}
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default function Game() {
+  const { home, away } = useStore();
+  const { players: homePlayers } = home;
+  const { players: awayPlayers } = away
+  return (
+    <main className={styles.court}>
+      <Scoreboard side="home" />
+      <div
+        className={[
+          styles.home__backcourt,
+          styles.court_area,
+          styles.backcourt,
+        ].join(" ")}
+      >
+        <div className={[styles.player, styles.player__home].join(" ")}>
+          {homePlayers[0]}
+        </div>
+        <div className={[styles.player, styles.player__home].join(" ")}>
+          {homePlayers[5]}
+        </div>
+        <div className={[styles.player, styles.player__home].join(" ")}>
+          {homePlayers[4]}
+        </div>
+      </div>
+      <div
+        className={[
+          styles.home__frontcourt,
+          styles.court_area,
+          styles.frontcourt,
+        ].join(" ")}
+      >
+        <div className={[styles.player, styles.player__home].join(" ")}>
+          {homePlayers[1]}
+        </div>
+        <div className={[styles.player, styles.player__home].join(" ")}>
+          {homePlayers[2]}
+        </div>
+        <div className={[styles.player, styles.player__home].join(" ")}>
+          {homePlayers[3]}
+        </div>
+      </div>
+      <div
+        className={[
+          styles.away__frontcourt,
+          styles.court_area,
+          styles.frontcourt,
+        ].join(" ")}
+      >
+        <div className={[styles.player, styles.player__away].join(" ")}>
+          {awayPlayers[3]}
+        </div>
+        <div className={[styles.player, styles.player__away].join(" ")}>
+          {awayPlayers[2]}
+        </div>
+        <div className={[styles.player, styles.player__away].join(" ")}>
+          {awayPlayers[1]}
+        </div>
+      </div>
+      <div
+        className={[
+          styles.away__backcourt,
+          styles.court_area,
+          styles.backcourt,
+        ].join(" ")}
+      >
+        <div className={[styles.player, styles.player__away].join(" ")}>
+          {awayPlayers[4]}
+        </div>
+        <div className={[styles.player, styles.player__away].join(" ")}>
+          {awayPlayers[5]}
+        </div>
+        <div className={[styles.player, styles.player__away].join(" ")}>
+          {awayPlayers[0]}
+        </div>
+      </div>
+      <Scoreboard side="away" />
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 import styles from "./page.module.css";
 
-import { create } from 'zustand'
+import { create } from 'zustand';
+import Link from "next/link";
 
 type Player = 'S' | 'OH1' | 'MB1' | 'OP' | 'OH2' | 'MB2';
 
@@ -22,7 +23,7 @@ const useStore = create<GameStore>((set) => ({
   homeScore: 0,
   awayScore: 0,
   servingSide: 'home',
-  rotatePlayers: (side='home', forward=true) => set((state) => {
+  rotatePlayers: (side, forward=true) => set((state) => {
     const playerKeyname: 'homePlayers' | 'awayPlayers' = `${side}Players`;
     let players = [...state[playerKeyname]];
     if (forward) {
@@ -60,158 +61,8 @@ export default function Home() {
 
 
   return (
-    <main className={styles.court}>
-      <div className={styles.scoreboard}>
-        <button className={styles.scoreboard__button} onClick={() => updateScore('sub', 'home')}>-</button>
-        <p className={styles.scoreboard__score}>{homeScore}</p>
-        <button className={styles.scoreboard__button} onClick={() => updateScore('add', 'home')}>+</button>
-      </div>
-      <div
-        className={[
-          styles.home__backcourt,
-          styles.court_area,
-          styles.backcourt,
-        ].join(" ")}
-        >
-        <div
-          className={[
-            styles.player,
-            styles.player__home,
-            styles.position__left,
-          ].join(" ")}
-        >
-          {homePlayers[0]}
-        </div>
-        <div
-          className={[
-            styles.player,
-            styles.player__home,
-            styles.position__middle,
-          ].join(" ")}
-        >
-          {homePlayers[5]}
-        </div>
-        <div
-          className={[
-            styles.player,
-            styles.player__home,
-            styles.position__right,
-          ].join(" ")}
-        >
-          {homePlayers[4]}
-        </div>
-      </div>
-      <div
-        className={[
-          styles.home__frontcourt,
-          styles.court_area,
-          styles.frontcourt,
-        ].join(" ")}
-      >
-        <div
-          className={[
-            styles.player,
-            styles.player__home,
-            styles.position__left,
-          ].join(" ")}
-        >
-          {homePlayers[1]}
-        </div>
-        <div
-          className={[
-            styles.player,
-            styles.player__home,
-            styles.position__middle,
-          ].join(" ")}
-        >
-          {homePlayers[2]}
-        </div>
-        <div
-          className={[
-            styles.player,
-            styles.player__home,
-            styles.position__right,
-          ].join(" ")}
-        >
-          {homePlayers[3]}
-        </div>
-      </div>
-      <div
-        className={[
-          styles.away__frontcourt,
-          styles.court_area,
-          styles.frontcourt,
-        ].join(" ")}
-      >
-        <div
-          className={[
-            styles.player,
-            styles.player__away,
-            styles.position__left,
-          ].join(" ")}
-        >
-          {awayPlayers[3]}
-        </div>
-        <div
-          className={[
-            styles.player,
-            styles.player__away,
-            styles.position__middle,
-          ].join(" ")}
-        >
-          {awayPlayers[2]}
-        </div>
-        <div
-          className={[
-            styles.player,
-            styles.player__away,
-            styles.position__right,
-          ].join(" ")}
-        >
-          {awayPlayers[1]}
-        </div>
-      </div>
-      <div
-        className={[
-          styles.away__backcourt,
-          styles.court_area,
-          styles.backcourt,
-        ].join(" ")}
-      >
-        <div
-          className={[
-            styles.player,
-            styles.player__away,
-            styles.position__left,
-          ].join(" ")}
-        >
-          {awayPlayers[4]}
-        </div>
-        <div
-          className={[
-            styles.player,
-            styles.player__away,
-            styles.position__middle,
-          ].join(" ")}
-        >
-          {awayPlayers[5]}
-        </div>
-        <div
-          className={[
-            styles.player,
-            styles.player__away,
-            styles.position__right,
-          ].join(" ")}
-        >
-          {awayPlayers[0]}
-        </div>
-
-      </div>
-      <div className={styles.scoreboard}>
-        <button className={styles.scoreboard__button} onClick={() => updateScore('sub', 'away')}>-</button>
-        <p className={styles.scoreboard__score}>{awayScore}</p>
-        <button className={styles.scoreboard__button} onClick={() => updateScore('add', 'away')}>+</button>
-      </div>
-    </main>
+    <Link href={'/game'}>
+      <h1>Go to game</h1>
+    </Link>
   );
 }
